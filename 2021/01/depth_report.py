aee3a08f4637d2b87e792c449462372fc4ef7043
Vladimir Konushliev <v.konushliev@gmail.com>
2021-12-03 17:52:24 +0200

    Polish first puzzle's Python solution

--- 2021/01/depth_report.py
+++ 2021/01/depth_report.py
@@ -1,11 +1,9 @@
 def read_input():
-    return list(map(lambda i: int(i), open('../input/01.txt', 'r').read().split()))
+    with open('../input/01.txt') as f:
+        return [int(line) for line in f.read().split()]
 
 def increases(input):
-    result = 0
-    for a, b in zip(input, input[1:]):
-        if a < b: result += 1
-    return result
+    return sum(1 for a,b in zip(input, input[1:]) if a < b)
 
 def three_m_sliding_increases(input):
     windows = [sum(input[i:i+3]) for i in range(0, len(input)-2)]
@@ -13,8 +11,8 @@ def three_m_sliding_increases(input):
 
 def main():
     input = read_input()
-    print increases(input)
-    print three_m_sliding_increases(input)
+    print(increases(input))
+    print(three_m_sliding_increases(input))
 
 if __name__ == "__main__":
     main()
