1e7294272cb43cf2c20be00c0349b2c7af0e99fd
Vladimir Konushliev <v.konushliev@gmail.com>
2021-12-21 09:22:49 +0200

    fixup! 21: First part

--- 2021/21/dice.rb
+++ 2021/21/dice.rb
@@ -4,7 +4,6 @@ FINISH = 1000
 
 def roll
   r = 3.times.map { @dice.next }
-  print " rolls #{r.join('+')} "
   r.sum
 end
 
@@ -12,24 +11,24 @@ def move(pos, draw)
   (pos + draw).then { _1 > 10 ? _1 % 10 : _1}.then { _1 == 0 ? 10 : _1 }
 end
 
-def game(p1, p2, s1, s2)
-  print "Player 1: from #{p1}"
-  p1 = move(p1, roll)
+def game(p1, p2, s1, s2, d1, d2)
+  print "Player 1: rolls #{d1} from #{p1}"
+  p1 = move(p1, d1)
   s1 += p1
   print " to #{p1} score #{s1}\n"
 
   if s1 >= FINISH
     p 'Player 1 wins'
   else
-    print "Player 2: from #{p2}"
-    p2 = move(p2, roll)
+    print "Player 2: rolls #{d2} from #{p2}"
+    p2 = move(p2, d2)
     s2 += p2
     print " to #{p2} score #{s2}\n"
 
     if s2 >= FINISH
       p 'Player 2 wins'
     else
-      game(p1, p2, s1, s2)
+      game(p1, p2, s1, s2, roll, roll)
     end
   end
 end
@@ -39,7 +38,7 @@ p1, p2 = input.map { _1.split(': ')[1].to_i }
 s1, s2 = 0, 0
 @dice = (1..100).to_a.cycle
 
-game(p1, p2, s1, s2)
+game(p1, p2, s1, s2, roll, roll)
 
 __END__
 Player 1 starting position: 4
