f334a29ed51e5e7339cc363135167ff5d6d38312
Vladimir Konushliev <v.konushliev@gmail.com>
2022-12-03 18:18:53 +0200

    Day 1

---
+++ 2022/01/solution.rb
@@ -0,0 +1,35 @@
+#!ruby
+input = (STDIN.tty? ? DATA : STDIN).readlines(chomp: true)
+
+sums = []
+current_sum = 0
+
+input.
+  each do |calories|
+    if calories.size != 0
+      current_sum += calories.to_i
+    else
+      sums << current_sum
+      current_sum = 0
+    end
+  end
+
+p sums.max
+p sums.sort.last(3).sum
+
+__END__
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+
